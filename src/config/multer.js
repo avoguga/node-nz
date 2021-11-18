0b5884b8a73f1ef9ecd53a6e80f676797225fa8d
O Gustavo <avoguga@gmail.com>
2021-11-18 04:04:02 -0300

    adding database

--- src/config/multer.js
+++ src/config/multer.js
@@ -1,10 +1,11 @@
 const multer = require('multer');
 const path = require('path');
 const crypto = require('crypto');
+const multerS3 = require('multer-s3');
+const aws = require('aws-sdk');
 
-module.exports = {
-    dest: path.resolve(__dirname, '..', '..', 'tmp', 'uploads'),
-    storage: multer.diskStorage({
+const storageTypes = {
+    local: multer.diskStorage({
         destination: (req, file, cb) => {
             cb(null, path.resolve(__dirname, '..', '..', 'tmp', 'uploads'))
         },
@@ -18,6 +19,16 @@ module.exports = {
             })
         },
     }),
+    s3: multerS3({
+        s3: new aws.S3(),
+        bucket: 'nz-mongo-user',
+        contentType: multerS3.AUTO_CONTENT_TYPE,
+    })
+}
+
+module.exports = {
+    dest: path.resolve(__dirname, '..', '..', 'tmp', 'uploads'),
+    storage: ,
     limits: {
         fileSize: 2 * 1024 * 1024,
     },
